87645ffc1bf536678c1d2074603fdbff25e6d937
Adam <s7adfish@uni-bonn.de>
2020-03-09 17:42:52 +0100

    httpRequestRedirection Case

--- src/main/java/httpRequestRedirections_SourceLevel0_TransformationLevel0_n_TargetLevel0/Source_Target.java
+++ src/main/java/httpRequestRedirections_SourceLevel0_TransformationLevel0_n_TargetLevel0/Source_Target.java
@@ -1,13 +1,9 @@
-package responseSendError_SourceLevel0_TrasformationLevel0_n_TargetLevel0;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Collection;
-import java.util.Locale;
+package httpRequestRedirections_SourceLevel0_TransformationLevel0_n_TargetLevel0;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 
 
@@ -27,18 +23,18 @@ public class Source_Target {
     	    throws ServletException, IOException {
     	        
     	    	//Case 1: Request Parameter wird in ein String geschrieben und vor der Verwendung desinfiziert.
-    	    	String requestParameter = request.getParameter("page");
+    	    	String requestParameter = request.getParameter("URL");
     	    	
-    	    	response.sendError(HttpServletResponse.SC_NOT_FOUND, requestParameter );
+    	    	response.sendRedirect(requestParameter );
     	    
     	        //Case 2: Request Parameter wird unmittelbar in der Methode sendError() des Objektes Response übergeben und rechts mit einem zusätzlichen String konkateniert.
-    	        response.sendError(HttpServletResponse.SC_NOT_FOUND, requestParameter + "was not found.");
+    	        response.sendRedirect( requestParameter + "was not found.");
 
     	        //Case 3: Request Parameter wird unmittelbar in der Methode sendError() des Objektes Response übergeben und links mit einem zusätzlichen String konkateniert.
-    	        response.sendError(HttpServletResponse.SC_NOT_FOUND, "Was not found: " + requestParameter );
+    	        response.sendRedirect( "Was not found: " + requestParameter );
 
     	        //Case 4: Request Parameter wird unmittelbar in der Methode sendError() des Objektes Response übergeben und links und rechts mit einem zusätzlichen String konkateniert.
-    	        response.sendError(HttpServletResponse.SC_NOT_FOUND, "Page " +requestParameter + " was not found!" );
+    	        response.sendRedirect( "Page " +requestParameter + " was not found!" );
     	        
     	    }
 
